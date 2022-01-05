96bb8af554e54e42b521b79703c216a981e040b6
zhouhuajian <zhouhuajian@email.com>
2022-01-05 11:59:01 +0800

    第02课 框架设计与首页访问

--- main.py
+++ main.py
@@ -4,7 +4,25 @@
 @author  : zhouhuajian
 @version : v1.0
 """
+from os.path import dirname
+
+from selenium import webdriver
+
+
+class ImageAutoSearchAndSave:
+    """图片自动搜索保存"""
+
+    def __init__(self):
+        """初始化"""
+        self.driver = webdriver.Chrome(executable_path=dirname(__file__) + '/chromedriver.exe')
+
+    def run(self):
+        """开始运行"""
+        print("========= 开始 =========")
+        # 访问首页
+        self.driver.get("https://pixabay.com/")
+        print("========= 结束 =========")
 
 
 if __name__ == '__main__':
-    print(123)
+    ImageAutoSearchAndSave().run()
